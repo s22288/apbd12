69e762719599933c75c581c9fa142bc46259df75
Michał Woźniak <s22288@pjwstk.edu.pl>
2023-01-13 14:37:09 +0100

    add-files

---
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.Property(e => e.Title).IsRequired();
+                entity.Property(e => e.Title).HasMaxLength(60);
+
+                entity.Property(e => e.ReleaseDate).HasColumnType("Date");
+                entity.Property(e => e.ReleaseDate).HasColumnName("Release Date");
+
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+                entity.Property(e => e.Price).HasColumnType("Money");
+
+                entity.Property(e => e.Genre).HasMaxLength(30);
+                entity.Property(e => e.Genre).IsRequired();
+
+                entity.Property(e => e.Rating).IsRequired();
+
+                entity.ToTable("Movie");
+            });
+
+
+        }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+               
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Star Trek",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Sci-fie",
+                        Rating = "R",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Star wars ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Sci-fie",
+                        Rating = "R",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Harry Potter",
+                        ReleaseDate = DateTime.Parse("1976-2-23"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Harry Potter 2",
+                        ReleaseDate = DateTime.Parse("1959-6-15"),
+                        Genre = "Western",
+                        Rating = "R",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
